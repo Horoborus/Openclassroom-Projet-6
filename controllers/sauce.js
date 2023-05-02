0149660f8794aac428cf9cd5ddb21a45184abedf
Filipe F <107125179+Horoborus@users.noreply.github.com>
2023-05-02 14:58:36 +0200

    controllers a jour

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,6 +1,7 @@
 const Sauce = require("../models/sauce.js");
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
+const user = require("../models/user.js");
 
 // fonction pour récupérer toutes les sauces
 exports.getSauces = (req, res, next) => {
@@ -51,44 +52,53 @@ exports.getOneSauce = (req, res, next) => {
 };
 
 // Modifier Sauce
-exports.modifySauce = (req, res, next) => {
+exports.createSauce = (req, res, next) => {
   const sauceBody = JSON.parse(req.body.sauce);
-  const sauceId = req.params.id;
+  delete sauceBody._id;
+  delete sauceBody._userId;
   const sauce = new Sauce({
     ...sauceBody,
+    userId: req.auth.userId,
     imageUrl: `${req.protocol}://${req.get("host")}/images/${
       req.file.filename
     }`,
-    likes: 0,
-    dislikes: 0,
-    usersLiked: [],
-    usersDisliked: [],
   });
 
-  Sauce.updateOne({ _id: sauceId }, sauce)
+  sauce
+    .save()
     .then(() => {
-      res.status(201).json({
-        message: "Sauce modifiée avec succès!",
-      });
+      res.status(201).json({ message: "Objet enregistré !" });
     })
     .catch((error) => {
-      res.status(400).json({
-        error: error,
-      });
+      res.status(400).json({ error });
     });
 };
 
 // Supprimer Sauce
+
 exports.deleteSauce = (req, res, next) => {
-  Sauce.deleteOne({ _id: req.params.id })
-    .then(() => {
-      res.status(200).json({
-        message: "Deleted!",
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      if (!sauce) {
+        return res.status(404).json({ message: "Sauce not found" });
+      }
+      if (sauce.userId !== req.auth.userId) {
+        return res.status(401).json({ message: "Not authorized" });
+      }
+      const filename = sauce.imageUrl.split("/images/")[1];
+      fs.unlink(`images/${filename}`, (error) => {
+        if (error) {
+          return res.status(500).json({ error });
+        }
+        sauce
+          .deleteOne({ _id: req.params.id })
+          .then(() => {
+            res.status(200).json({ message: "Objet supprimé !" });
+          })
+          .catch((error) => res.status(500).json({ error }));
       });
     })
     .catch((error) => {
-      res.status(400).json({
-        error: error,
-      });
+      res.status(500).json({ error });
     });
 };
